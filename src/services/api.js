11c4eb7d5d76dbcc64d14215d15edd770e6da595
STS-Engineer <administration.sts@avocarbon.com>
2025-12-21 23:05:07 +0100

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 // src/services/api.js
-const API_BASE_URL = 'http://localhost:4000/ajouter'; // Adjust to your backend URL
+const API_BASE_URL = 'https://plan-back.azurewebsites.net/ajouter'; // Adjust to your backend URL
 
 class ApiService {
   constructor() {
